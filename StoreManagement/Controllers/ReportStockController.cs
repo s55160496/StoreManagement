177eb8c3fff52ac71549dd5e0e2884e6e6be4a24
E14-06 <phongsawat99@gmail.com>
2022-02-01 11:25:52 +0700

    [Add report]

---
+++ StoreManagement/Controllers/ReportStockController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using StoreManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace StoreManagement.Controllers
+{
+    public class ReportStockController : BaseController
+    {
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        public ReportStockController(IConfiguration config, IWebHostEnvironment hostingEnvironment) : base(config)
+        {
+            this._hostingEnvironment = hostingEnvironment;
+        }
+        public IActionResult Index()
+        {
+            HttpStatusCode code = HttpStatusCode.OK;
+            var CUSTOMER = GET_TBM_CUSTOMER(out code, new TBM_CUSTOMER() { });
+            ViewData["CUSTOMER"] = CUSTOMER == null ? null : CUSTOMER.ToArray();
+
+            var EMPLOYEE = GET_TBM_EMPLOYEE(out code, new TBM_EMPLOYEE() { });
+            ViewData["EMPLOYEE"] = EMPLOYEE.ToArray();
+
+            var LOCATION = GET_TBM_LOCATION_STORE(out code, new TBM_LOCATION_STORE() { });
+            ViewData["LOCATION"] = LOCATION.ToArray();
+
+            return View();
+        }
+    }
+}
