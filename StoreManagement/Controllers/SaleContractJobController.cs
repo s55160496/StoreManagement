2b7fcfb8137d61631dd39939e11a014ad748f143
rangsanchipphang <rangsanchipphang@RANGSANCHIPB992>
2024-03-04 23:54:36 +0700

    add SaleContractJob

---
+++ StoreManagement/Controllers/SaleContractJobController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace StoreManagement.Controllers
+{
+    public class SaleContractJobController : BaseController
+    {
+        private string SaleContractJob = "http://203.151.136.81/SaleContractJob/Home/Link";
+        public SaleContractJobController(IConfiguration config) : base(config)
+        {
+        }
+        public IActionResult Index()
+        {
+
+            return Redirect($"{SaleContractJob}?token={SessionUserInfo().TOKEN}");
+        }
+    }
+}
